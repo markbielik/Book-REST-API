4edeaeb16ab427fd5e6865d1af0ea3ed36823388
Marek Grabowski <markgrabowskyy@gmail.com>
2022-05-24 21:37:42 +0200

    add api_spec/

--- optionsBook/urls.py
+++ optionsBook/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ListBooksView, DetailBookView
+from .views import ListBooksView, DetailBookView, ApiSpecification
 
 urlpatterns = [
+    path('api_spec/', ApiSpecification.as_view(), name='api_spec'),
     path('books/', ListBooksView.as_view(), name="books_list"),
     path('books/<int:pk>/', DetailBookView.as_view(), name="book_detail"),
+    
 ]
--- optionsBook/views.py
+++ optionsBook/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Books
 from .serializers import BookSerializer
@@ -25,3 +27,16 @@ class DetailBookView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_view_name(self):
         return "Book Detail"
+
+
+class ApiSpecification(APIView):
+    """
+    show a defined api version
+    """
+    def get(self, request):
+        data = {
+            "info": {
+                'version': "2022.05.16"
+            }
+        }
+        return Response(data)
