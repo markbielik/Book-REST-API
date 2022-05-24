12cdca33653728565319b340088cd32484bee356
Marek Grabowski <markgrabowskyy@gmail.com>
2022-05-24 21:01:31 +0200

    create new model Book

--- optionsBook/models.py
+++ optionsBook/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Books(models.Model):
+    external_id = models.CharField(verbose_name='external_id',
+                                   max_length=100,
+                                   blank=True)
+    title = models.CharField(verbose_name='title',
+                             max_length=255)
+    authors = models.CharField(verbose_name='authors',
+                               max_length=300)
+    publication_year = models.PositiveIntegerField(verbose_name='published_year')
+    acquired_state = models.BooleanField(verbose_name='acquired',
+                                         default=True)
+    thumbnail = models.CharField(verbose_name='thumbnail',
+                                 max_length=300,
+                                 blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail', args=(self.pk,))
+
